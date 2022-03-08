3c991a1f167b7c5a637a6a3aafedef3bf691fdb1
TJ Park <tjp1992@gmail.com>
2022-03-08 11:57:36 +0900

    Changed layout of frontend

--- app.py
+++ app.py
@@ -47,22 +47,25 @@ DEFAULT_START = nearest_business_day(DEFAULT_START)
 
 # ---------------stock selection------------------
 CRYPTO = np.array([ "BTC/USD", "ETH/USD", "LTC/USD"])
+MODEL_LI = np.array(['FB_PROPHET', 'SARIMAX', 'LSTM'])
 SYMB = window_selection_c.selectbox("select currency", CRYPTO)
-
+MODEL = window_selection_c.selectbox("select model", MODEL_LI)
 # chart_width = st.expander(label="chart width").slider("", 1000, 2800, 1400) #arguments: label, min, default, max
 
 # # # ------------------------Plot stock linechart--------------------
 
 fig=go.Figure()
 # crypto = Stock(symbol=SYMB)
-crypto = Crypto(SYMB.split('/')[0])
+crypto = Crypto(SYMB.split('/')[0], MODEL)
 # crypto.load_data(START, END, inplace=True)
-st.write(crypto.test_api())
-fig = crypto.plot_raw_data(fig)
+# fig = crypto.plot_raw_data(fig)
+fig = crypto.test_api()
 
 #---------------styling for plotly-------------------------
 fig.update_layout(
-            width=950,  # Change Chart width to Constant
+            xaxis_title='Date',
+            yaxis_title=crypto.symbol,
+            width=1250,  # Change Chart width to Constant
             margin=dict(l=0, r=0, t=0, b=0, pad=0),
             legend=dict(
                 x=0,
@@ -73,12 +76,21 @@ fig.update_layout(
             autosize=False,
             template="plotly_dark",
 )
-col1, col2 = st.columns([4,1])
-with col1:
-    st.write(fig)
-with col2:
-    st.image('https://alternative.me/images/fng/crypto-fear-and-greed-index-2022-3-3.png', width=300)
+st.write(fig)
 
-change_c = st.sidebar.container()
-with change_c:
-    crypto.test_api()
+
+col1, col2, col3= st.columns(3)
+with col1:
+    # Change the fear and grid to the last day of the dataset
+    st.image('https://alternative.me/images/fng/crypto-fear-and-greed-index-2022-3-3.png', width=300)
+with col2:
+    # Historical Data for the fear and greed
+    pass
+with col3:
+    # Description for the fear and greed
+    pass
+
+
+# change_c = st.sidebar.container()
+# with change_c:
+#     crypto.predict_model()
