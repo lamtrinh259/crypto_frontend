d634b4a8ad241363216aef8240fc35ab5177f191
TJ Park <tjp1992@gmail.com>
2022-03-10 18:00:58 +0900

    final adjustment to frontend

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ def predict():
                 yaxis_title='USD',
                 font=dict(size=17),
                 yaxis={'side': 'right'} ,
-                title= '<b>{} Prediction for {}/USD</b>'.format(crypto.model, crypto.symbol),
+                title= '<b>{} Forecast for {}/USD</b>'.format(model_display[model_sel], crypto.symbol),
                 title_x=0.5,
                 # width=1450,  # Change Chart width to Constant
                 # height=800, # leave commented out for default column size
@@ -103,7 +103,7 @@ def predict():
     )
 
 
-    col1, col2, col3= st.columns(3)
+    col1, col2= st.columns(2)
     with col1:
         # Change the fear and grid to the last day of the dataset
         st.write(' ')
@@ -111,16 +111,6 @@ def predict():
         last_date = '{dt.year}-{dt.month}-{dt.day}'.format(dt = last_date)
         st.image('https://alternative.me/images/fng/crypto-fear-and-greed-index-{}.png'.format(last_date))
     with col2:
-        # Historical Data for the fear and greed
-        st.write('### Why Measure Fear and Greed?')
-        st.write('The fear and greed index is a composite score number that is calculated based on these 6 factors surrounding Bitcoin (corresponding weights inside parentheses): ')
-        st.markdown('- volatility and maximum drawdown of Bitcoin (25%)\n- market momentum/volume (25%)\n- social media (15%)\n- surveys (15%)\n- dominance (10%)\n- trends (10%)')
-        st.write('A zero score means **Extreme Fear**, while 100 means **Extreme Greed**. If the score is between 46 and 54, then the market is feeling neutral. In general, while the market is in **extreme fear** it signifies a good opportunity to **buy**. On the other hand, if the market is feeling extremely greedy (close to 100), then it signifies that there may be a correction (prices will come down) in the near future. Credits go to alternative.me for providing this index.')
-        # st.write('The crypto market behaviour is very emotional. People tend to get greedy when the market is rising which results in **FOMO** (Fear of missing out). Also, people often sell their coins in irrational reaction of seeing red numbers. With our Fear and Greed Index, we try to save you from your own emotional overreactions. There are two simple assumptions:')
-        # st.write('`Extreme fear` can be a sign that investors are too worried. That could be a buying opportunity.')
-        # st.write('##### When Investors are getting too greedy, that means the market is due for a correction.')
-        # st.write('Therefore, we analyze the current sentiment of the Bitcoin market and crunch the numbers into a simple meter from 0 to 100. Zero means `Extreme Fear`, while 100 means `Extreme Greed`. See below for further information on our data sources.')
-    with col3:
         # Description for the fear and greed
         st.write('### {} Predictions'.format(MODEL))
 
@@ -137,10 +127,19 @@ def predict():
             file_name='predictions.csv',
             mime='text/csv',
         )
+    # Historical Data for the fear and greed
+    st.write('### Why Measure Fear and Greed?')
+    st.write('The fear and greed index is a composite score number that is calculated based on these 6 factors surrounding Bitcoin (corresponding weights inside parentheses): ')
+    st.markdown('- volatility and maximum drawdown of Bitcoin (25%)\n- market momentum/volume (25%)\n- social media (15%)\n- surveys (15%)\n- dominance (10%)\n- trends (10%)')
+    st.write('A zero score means **Extreme Fear**, while 100 means **Extreme Greed**. If the score is between 46 and 54, then the market is feeling neutral. In general, while the market is in **extreme fear** it signifies a good opportunity to **buy**. On the other hand, if the market is feeling extremely greedy (close to 100), then it signifies that there may be a correction (prices will come down) in the near future. Credits go to alternative.me for providing this index.')
+    # st.write('The crypto market behaviour is very emotional. People tend to get greedy when the market is rising which results in **FOMO** (Fear of missing out). Also, people often sell their coins in irrational reaction of seeing red numbers. With our Fear and Greed Index, we try to save you from your own emotional overreactions. There are two simple assumptions:')
+    # st.write('`Extreme fear` can be a sign that investors are too worried. That could be a buying opportunity.')
+    # st.write('##### When Investors are getting too greedy, that means the market is due for a correction.')
+    # st.write('Therefore, we analyze the current sentiment of the Bitcoin market and crunch the numbers into a simple meter from 0 to 100. Zero means `Extreme Fear`, while 100 means `Extreme Greed`. See below for further information on our data sources.')
 
 
 def index_page():
-    st.markdown("<h1 style='text-align: center; color: white;'>Welcome To Crypto Predict</h1>", unsafe_allow_html=True)
+    st.markdown("<h1 style='text-align: center; color: white;'>Welcome To Crypto Forecast</h1>", unsafe_allow_html=True)
     # Welcome to the world of cryptocurrency prices forecasting. We are a group of crypto enthusiasts who just happen to also know about data science and machine learning. We created this app for users like you that are interested in cryptocurrency, and want to gather more information about some of the crypto assets in the top 100 (by market capitalization) so that you can make your own informed decisions. You can begin by selecting the crypto asset of interest and the corresponding forecast method in the left panel.
     st.markdown('Welcome to the world of cryptocurrency prices forecasting.\n We are a group of crypto enthusiasts who just happen to also know about data science and machine learning. We created this app for users like you that are interested in cryptocurrency, and want to gather more information about some of the crypto assets in the top 100 (by market capitalization) so that you can make your own informed decisions. ')
     st.markdown('You can begin by selecting the crypto asset of interest and the corresponding forecast method in the left panel.')
@@ -157,7 +156,7 @@ def index_page():
 # ------ layout setting---------------------------
 window_selection_c = st.sidebar.container() # create an empty container in the sidebar
 # st.write('<style>div.Widget.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
-state = window_selection_c.radio('',('Home', 'Predict'))
+state = window_selection_c.radio('',('Home', 'Forecast'))
 
 window_selection_c.markdown("## Currency Selection") # add a title to the sidebar container
 # sub_columns = window_selection_c.columns(2) #Split the container into two columns for start and end date
@@ -172,11 +171,27 @@ DEFAULT_START=YESTERDAY - datetime.timedelta(days=700)
 
 
 # ---------------stock selection------------------
-
-CRYPTO = np.array([ "BTC/USD", "ETH/USD", "LTC/USD"])
-MODEL_LI = np.array(['FB_PROPHET', 'SARIMAX', 'LSTM'])
-SYMB = window_selection_c.selectbox("Currency", CRYPTO)
-MODEL = window_selection_c.selectbox("Model", MODEL_LI)
+symb_map = {
+    'Bitcoin(USD)':'BTC',
+    'Ethereum(USD)':'ETH',
+    'LiteCoin(USD)':'LTC'
+}
+model_map = {
+    'Short-Term (Prophet)':'FB_PROPHET',
+    'Short-Term (SARIMAX)':'SARIMAX',
+    'Long-Term  (LSTM)':'LSTM'
+}
+model_display = {
+    'Short-Term (Prophet)':'Prophet',
+    'Short-Term (SARIMAX)':'SARIMAX',
+    'Long-Term  (LSTM)':'LSTM'
+}
+CRYPTO = np.array([ "Bitcoin(USD)", "Ethereum(USD)", "LiteCoin(USD)"])
+MODEL_LI = np.array(['Short-Term (Prophet)', 'Short-Term (SARIMAX)', 'Long-Term  (LSTM)'])
+symbol_sel = window_selection_c.selectbox("Currency", CRYPTO)
+model_sel = window_selection_c.selectbox("Model", MODEL_LI)
+SYMB = symb_map[symbol_sel]
+MODEL = model_map[model_sel]
 # window_selection_c.radio('<style>div.Widget.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
 # clicked = window_selection_c.button('Predict')
 
@@ -184,15 +199,15 @@ MODEL = window_selection_c.selectbox("Model", MODEL_LI)
 if state == 'Home':
     index_page()
     window_selection_c.write('### Instructions')
-    window_selection_c.write('1. Select Currency to Predict')
-    window_selection_c.write('2. Select Model to use to Predict')
-    window_selection_c.write('3. Invest According to the Predictions')
+    window_selection_c.write('1. Select currency to forecast')
+    window_selection_c.write('2. Select model to use to forecast')
+    window_selection_c.write('3. Make your own informed decision')
 
 else:
     predict()
     window_selection_c.write('### Instructions')
-    window_selection_c.write('1. Select Currency to Predict')
-    window_selection_c.write('2. Select Model to use to Predict')
-    window_selection_c.write('3. Invest According to the Predictions')
+    window_selection_c.write('1. Select currency to forecast')
+    window_selection_c.write('2. Select model to use to forecast')
+    window_selection_c.write('3. Make your own informed decision')
 
 # chart_width = st.expander(label="chart width").slider("", 1000, 2800, 1400) #arguments: label, min, default, max
